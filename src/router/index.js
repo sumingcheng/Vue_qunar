9f459319052464e2f95b83fc6be9669cec82a8f9
Your Name <fanxiaohaohaha@qq.com>
2021-08-25 17:36:34 +0800

    2021年8月25日 17:36:33

--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,12 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      // 访问的根目录 http://localhost:8080/#/
+      // 也就是说让跟路由展示我们的 HelloWorld
       path: '/',
+      // 展示的组件内容
       name: 'HelloWorld',
+      // 进行HelloWorld的组件注册
       component: HelloWorld
     }
   ]
