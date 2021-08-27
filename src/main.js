58c5b3fd3c2379bd9ace9cfc96ed19f221962191
Your Name <fanxiaohaohaha@qq.com>
2021-08-27 17:27:09 +0800

    2021年8月27日 17:27:08

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import FastClick from 'fastclick'
 import App from './App'
-import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import FastClick from 'fastclick'
+import router from './router' 
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 FastClick.attach(document.body);
 
 /* eslint-disable no-new */
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,31 @@
+<!-- The ref attr used to find the swiper instance -->
+<template>
+  <swiper ref="mySwiper" :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide>I'm Slide 1</swiper-slide>
+    <swiper-slide>I'm Slide 2</swiper-slide>
+    <swiper-slide>I'm Slide 3</swiper-slide>
+    <swiper-slide>I'm Slide 4</swiper-slide>
+    <swiper-slide>I'm Slide 5</swiper-slide>
+    <swiper-slide>I'm Slide 6</swiper-slide>
+    <swiper-slide>I'm Slide 7</swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination" slot="pagination"></div>
+    <div class="swiper-scrollbar" slot="scrollbar"></div>
+  </swiper>
+</template>
+
+<script>
+  export default {
+    name: 'HomeSwiper',
+    data: function() {
+      return {
+        swiperOption: {
+        }
+      }
+    },
+  }
+</script>
+
+<style lang="stylus" scope>
+</style>
