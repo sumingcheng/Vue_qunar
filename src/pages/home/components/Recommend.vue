36d5d5e180541f2c8df244b98ce24a99390241a6
Your Name <fanxiaohaohaha@qq.com>
2021-08-30 17:50:28 +0800

    2021年8月30日

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="recommend_title">
+    猜你喜欢
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Homecommend'
+}
+</script>
+
+<style scoped>
+.recommend_title{
+  width: 100%;
+  height: 0.8rem;
+  background-color: #eee;
+}
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -10,6 +11,8 @@
 import HomeHeader from "./components/Header.vue";
 import HomeSwiper from "./components/Swiper.vue";
 import HomeIcons from "./components/Icons";
+import HomeRecommend from "./components/Recommend";
+
 
 export default {
   name: "Home",
@@ -17,6 +20,7 @@ export default {
     "home-header": HomeHeader,
     "home-swiper": HomeSwiper,
     "home-icons":HomeIcons,
+    "home-recommend":HomeRecommend,
   },
 };
 </script>
